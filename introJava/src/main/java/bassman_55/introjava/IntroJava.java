2c6255dd6ebb65e0d04a702b5f36922cd868dda3
Lucas Molina <lucasivanmolina_bass@hotmail.com>
2023-03-08 21:32:09 -0300

    Update IntroJava.java
    Edit de los comentarios del archivo

--- introJava/src/main/java/bassman_55/introjava/IntroJava.java
+++ introJava/src/main/java/bassman_55/introjava/IntroJava.java
@@ -19,15 +19,19 @@ public class IntroJava {
         //guardamos en nombre lo que leemos en el ingreso de datos
         nombre = leer.next();
         System.out.println("Hola Mundo! soy " +nombre+ " y estoy programando en Java!");
+        */
 //---------------------------------------------------------------------------------------------------------------
+        /*
         Ejercicio de la guía T de definir diferentes variables
          
         int numero;
         boolean ver;
         String nomb2;
         float decimal;
+        */
 //---------------------------------------------------------------------------------------------------------------
-        EJ 1 - Escribir un programa que pida dos números enteros por teclado y calcule
+        /* 
+        //EJ 1 - Escribir un programa que pida dos números enteros por teclado y calcule
         la suma de los dos. El programa deberá después mostrar el resultado de la suma
         
       
@@ -38,17 +42,19 @@ public class IntroJava {
         int num2 = leer.nextInt();
         int suma = num1 + num2;
         System.out.println("El valor de la suma es: " + suma);
-        
+        */
 //--------------------------------------------------------------------------------------------------------------
+        /*
         //EJ 2 - Escribir un programa que pida tu nombre, lo guarde en una variable y lo muestre por pantalla
         
         String nombre;
         System.out.println("Ingresa tu nombre");
         nombre = leer.next();
         System.out.println("Hola " + nombre + " estás programando en Java");
-        
+        */
         
 //---------------------------------------------------------------------------------------------------------------
+        /*
         //EJ 3 - Escribir un programa que pida una frase y la muestre toda en mayúsculas y después toda en minúsculas.
         //Nota: investigar la función toUpperCase() y toLowerCase() en Java.
         
@@ -57,9 +63,9 @@ public class IntroJava {
         frase = leer.nextLine();
         System.out.println(frase.toUpperCase());
         System.out.println(frase.toLowerCase());
-        
+        */
 //--------------------------------------------------------------------------------------------------------------
-
+        /*
         //EJ 4 - Dada una cantidad de grados centígrados se debe mostrar su equivalente en grados Fahrenheit. 
         //La fórmula correspondiente es: F = 32 + (9 * C / 5)
     
@@ -67,8 +73,9 @@ public class IntroJava {
         float C = leer.nextFloat();
         float F = 32 + (9 * C / 5);
         System.out.println(F);
-    */
+        */
 //----------------------------------------------------------------------------------------------------------------
+        /*
         //EJ 5 - Escribir un programa que lea un número entero por teclado y muestre por pantalla el doble, 
         //el triple y la raíz cuadrada de ese número.
         //Nota: investigar la función Math.sqrt().
@@ -80,7 +87,7 @@ public class IntroJava {
         System.out.println(doble);
         System.out.println(triple);
         System.out.println(Math.sqrt(num));
-        
+        */
     
     }
     
